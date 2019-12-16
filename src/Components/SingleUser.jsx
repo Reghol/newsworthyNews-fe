bd7d75bea77b35bdbe8c3c57f06ab6a4b95b2346
Reghol <jakubnc@protonmail.com>
2019-12-16 12:02:40 +0000

    link connects to correct user in users tab

--- src/Components/SingleUser.jsx
+++ src/Components/SingleUser.jsx
@@ -2,18 +2,36 @@ import React, { Component } from "react";
 import * as api from "../Utils/api";
 class SingleUser extends Component {
   state = {
-    user: "",
+    username: null,
+    name: null,
+    avatar: null,
     isLoading: true
   };
 
   componentDidMount() {
-    api.getUserByUsername().then(({ users }) => {
-      this.setState({ users, isLoading: false });
+    const { username } = this.props;
+
+    api.getUserByUsername(username).then(({ data }) => {
+      const { username, name, avatar_url } = data.user;
+      this.setState({ username, name, avatar_url, isLoading: false });
     });
   }
 
   render() {
-    return <div>This is the user's page</div>;
+    const { username, name, avatar_url } = this.state;
+    return (
+      <div>
+        <p>Username: {username}</p>
+        <p>Name: {name}</p>
+        <p>Avatar:</p>
+        <img
+          alt="bla bla bla"
+          style={{ height: "50%", width: "50%" }}
+          src={avatar_url}
+        ></img>
+        <div>Articles</div>
+      </div>
+    );
   }
 }
 
--- src/Components/UsersList.jsx
+++ src/Components/UsersList.jsx
@@ -33,29 +33,33 @@ export default class UsersList extends Component {
       );
 
     return (
-      <div className="">
-        {users.map(user => {
-          return (
-            <main className="users-card" key={user.username}>
+      <>
+        <div className="">
+          {users.map(user => {
+            return (
               <Link
+                key={`/users/${user.username}`}
                 style={{ textDecoration: "none" }}
                 to={`/users/${user.username}`}
               >
-                <h3 className="users-name">
-                  {user.name} - <i className="users-name">{user.username}</i>
-                </h3>
+                <main className="users-card" key={user.username}>
+                  <h3 className="users-name">
+                    {user.name} - <i className="users-name">{user.username}</i>
+                  </h3>
+
+                  <img
+                    className="users-image"
+                    src={user.avatar_url}
+                    alt={`${user.username} pict`}
+                    height="42"
+                    width="42"
+                  />
+                </main>
               </Link>
-              <img
-                className="users-image"
-                src={user.avatar_url}
-                alt={`${user.username} pict`}
-                height="42"
-                width="42"
-              />
-            </main>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </>
     );
   }
 }
